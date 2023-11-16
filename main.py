c0be7108c0427c03e1d944841686c81dba6f4182
Александр В. Гуляев <agulyaev@jet.msk.su>
2023-11-16 17:26:48 +0300

    Added mac / arp processing

--- main.py
+++ main.py
@@ -1,16 +1,131 @@
-# This is a sample Python script.
+import argparse
+import out_to_screen
+import outintofiles
+import check_duplicates
+import txtfsmparsers
+import datamodel
+import os
+import pathlib
+import regparsers
+import analytics
+import copy
+from alive_progress import alive_bar
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+compliance_result = []
+devices = []
+empty_device = {}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def createparser():
+    parser = argparse.ArgumentParser(prog='CiscoParser', description='Utility for analyzing network device configurations v0.6.', epilog='author: asha77@gmail.com')
+    parser.add_argument('-d', '--configdir', required=False, help='Specify directory with cisco config files', type=pathlib.Path)
+    parser.add_argument('-c', '--compcheck', required=False, help='Perform compliance check on config files', action='store_true')
+    parser.add_argument('-u', '--disablefilecheck', required=False, help='Disable checking for unique devices config files', action='store_true')
+    parser.add_argument('-e', '--extractdata', required=False, help='Perform extraction of data from configs and diagnostic commands and draw picture', action='store_true')
+    return parser
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    parser = createparser()
+    namespace = parser.parse_args()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    curr_path = os.path.abspath(os.getcwd())
+    if namespace.configdir:
+        os.chdir(namespace.configdir)
+
+    if namespace.extractdata is True:
+        # get list of files with configs and diagnostic
+        list_of_files = os.listdir(namespace.configdir)
+        print("Starting processing files in folder: " + str(namespace.configdir))
+
+        # create output files where we will save results
+        outintofiles.init_files()
+
+        # check file duplicates using extracted serial numbers
+        if namespace.disablefilecheck is False:
+            if not check_duplicates.check_config_duplicates(list_of_files):
+                quit()
+        else:
+            print('!!!  ---- Unique files check is disabled ---- !!!')
+
+        # Start processing of configs
+        with alive_bar(len(list_of_files), length=55, title='Progress', force_tty=True) as bar:
+            for file in list_of_files:
+                bar.text = f'Processing file: {file}, please wait...'
+                if os.path.isfile(file):
+                    with open(file, "r", encoding='utf-8') as conffile:
+                        config = conffile.read()
+                        empty_device = copy.deepcopy(datamodel.config_entity)
+
+                        # get basic parameters
+                        regparsers.fill_devinfo_to_model_from_config(empty_device, config, file)   # add to model
+
+                        txtfsmparsers.get_esi_to_model(empty_device, config, curr_path)
+
+                        txtfsmparsers.get_macs_to_model(empty_device, config, curr_path)
+
+                        txtfsmparsers.get_arps_to_model(empty_device, config, curr_path)
+
+                        # get list of cdp neighbours
+# not yet                       txtfsmparsers.get_cdp_neighbours_to_model(empty_device, config, curr_path)
+
+                        # get list of ports with many MAC-addresses under them
+                        # outintofiles.many_macs_file_output(config, curr_path, cdp_neighbours, devinfo)  # optional - to rework
+
+                        # get interfaces configuration
+# not yet                       txtfsmparsers.get_interfaces_config_to_model(empty_device, config, curr_path)
+
+# not yet                       txtfsmparsers.get_vlans_configuration_to_model(empty_device, config, curr_path)
+
+                        # add collected device data into array
+                        devices.append(empty_device)
+                bar()
+
+        # print inventory data to screen and into cparser.csv file
+        out_to_screen.print_devices_summary(devices)
+        outintofiles.summary_file_output(devices)
+
+        # print all macs to file all_macs.csv
+        outintofiles.macs_to_file(devices)
+        # print all arps to file all_arps.csv
+        outintofiles.arps_to_file(devices)
+
+        ############ DO ANALYTICS ################
+        same_stated_macs, absent_macs = analytics.compare_macs(devices, "TC-YAR-MD6-FAB-SWL03", "TC-YAR-MD6-FAB-SWL04")
+        incompleted_arps, absent_arps = analytics.compare_arps(devices, "TC-YAR-MD6-FAB-SWL03", "TC-YAR-MD6-FAB-SWL04")
+
+        outintofiles.same_stated_macs_to_file(same_stated_macs, "_SWL03_SWL04")
+        outintofiles.absent_macs_to_file(absent_macs, "_SWL03_SWL04")
+
+        outintofiles.incompleted_arps_to_file(incompleted_arps, "_SWL03_SWL04")
+        outintofiles.absent_arps_to_file(absent_arps, "_SWL03_SWL04")
+
+
+        #  print all neighbours from all devices into 'all_neighbours_output.csv' file
+# not yet        outintofiles.all_neighbours_to_file(devices)
+
+        #  print links (connectivity) to all neighbours from all devices into file 'cdp_nei_output.csv'
+# not yet        outintofiles.connectivity_to_file(devices)
+
+        # print interfaces info into file
+# not yet        outintofiles.interfaces_to_file(devices)
+
+        # Trying to find missed devices that can be found in cdp data and save this to "missed_devices.csv" file
+# not yet        missed_devices = outintofiles.find_missed_devices()
+# not yet        outintofiles.missed_devices_file_output(missed_devices)
+
+#        if len(missed_devices) > 0:
+#            print('In CDP configuration we found mentioned {} devices, for whom we have no configurations.\n'
+#                  'See file \"missed.devices.csv\"'.format(len(missed_devices)))
+# analysis of required VLAN on trunk ports
+#        trunking_analisys()
+
+        print("Finished processing files in folder: " + str(namespace.configdir) + '\n')
+
+        # print devices summary
+        out_to_screen.tbl_files_info_out2scr()
+
+
+if __name__ == "__main__":
+    main()
